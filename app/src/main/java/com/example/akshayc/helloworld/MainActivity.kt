580a7c186db7eaf8df6ea6244f7123c7d61c6446
Akshay <s1558717@sms.ed.ac.uk>
2017-10-13 16:57:10 +0100

    Initial commit

---
+++ app/src/main/java/com/example/akshayc/helloworld/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.akshayc.helloworld
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        HelloWorld.setOnClickListener{ toast("Hello there")}
+
+    }
+}
